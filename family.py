e00ae05369c0e8cee5251119ea601026c4337748
python10101 <sonali_gaitonde@LAPTOP-J4KD9GTN.localdomain>
2018-06-18 00:47:40 +0530

    move file

---
+++ family.py
@@ -0,0 +1,21 @@
+#Family
+
+name=input("Enter your name: ")
+surname=input("Enter your surname: ")
+father_name=input("Enter your father's name: ")
+mother_name=input("Enter your mother's name: ")
+
+print(surname,end='.')
+print(name,end='&')
+print(father_name,end='@')
+print(mother_name)
+
+
+print(surname,end='>\n')
+print(name,end='@\n')
+print(father_name,end='#\n')
+print(mother_name,end='%\n')
+
+
+      
+
