eb158e4d7301300be84a7ac012f46e9264025c2f
python10101 <sonali_gaitonde@LAPTOP-J4KD9GTN.localdomain>
2018-06-18 02:06:07 +0530

    This program takes two inputs and prints if the first number is greater than, less than or equal to the second number.

---
+++ compofnumbers.py
@@ -0,0 +1,11 @@
+#Comparison of two numbers
+
+a=eval(input("Enter one number: "))
+b=eval(input("Enter another number: "))
+if a>b:
+            print("a is greater than b")
+elif b>a:
+            print("b is greater than a")
+else:
+            print("a is equal to b")
+
