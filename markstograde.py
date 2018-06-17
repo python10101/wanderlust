b862a88f23bdfc1e6c46b0e351d65ec3f74a3a41
python1010 <sonali_gaitonde@LAPTOP-J4KD9GTN.localdomain>
2018-06-18 01:55:59 +0530

    This program converts marks entered by the user to corresponding grades and displays it.

---
+++ markstograde.py
@@ -0,0 +1,20 @@
+# Marks to grade conversion
+
+a=eval(input("Enter your marks: "))
+print("Your grade is: ")
+if a>90:
+            print("O")
+elif a>80 and a<90:
+            print("A")
+elif a>70 and a<80:
+            print("B")
+elif a>60 and a<70:
+            print("C")
+elif a>50 and a<60:
+            print("D")
+elif a>40 and a<50:
+            print("E")
+elif a<40:
+            print("F")
+
+
