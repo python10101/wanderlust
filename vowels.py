e47870ed3a95ed0d2ae1cc5aefcd64ba032aeb5b
python10101 <sonali_gaitonde@LAPTOP-J4KD9GTN.localdomain>
2018-06-18 02:23:25 +0530

    This program gives the count of vowels in a string.

---
+++ vowels.py
@@ -0,0 +1,8 @@
+string=input("Enter a string: ")
+vowels=0
+for i in string:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='O' or i=='U' or i=='I'):
+        vowels = vowels+1
+print("Number of vowels are: ")
+print(vowels)
+
